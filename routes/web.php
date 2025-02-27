af842428b399c4cf4a481841f6102e7a8daf3b29
JustinG <justin.gestiere@gmail.com>
2025-02-27 15:52:36 +0100

    template

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.main');
 });
 
 Route::get('/restaurants', [RestaurantController::class, 'index'])->name('restaurants.index');
