1877f3a4d30b1060e301bb1aa21078a6782486b2
JustinG <justin.gestiere@gmail.com>
2025-02-27 12:13:03 +0100

    test

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,7 +10,7 @@
 </html> -->
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
