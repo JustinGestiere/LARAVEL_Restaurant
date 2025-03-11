99dc85d76177cd907f1fec4070a20d7101bf605d
JustinG <justin.gestiere@gmail.com>
2025-03-11 08:17:40 +0100

    header

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,7 +1,8 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="fr">
 @include('layout.head')
-<body>
+
+<!-- <body>
     
     @yield('main')
 
@@ -9,11 +10,11 @@
 </body>
 </html> -->
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="fr">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
+     Meta, title, CSS, favicons, etc. 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,25 +22,25 @@
 
     <title>Gentelella</title>
 
-    <!-- Bootstrap -->
+     Bootstrap 
     <link href="{{ asset('assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
-    <!-- Font Awesome -->
+     Font Awesome 
     <link href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
-    <!-- NProgress -->
+    NProgress 
     <link href="{{ asset('assets/vendors/nprogress/nprogress.css') }}" rel="stylesheet">
-    <!-- iCheck -->
+    iCheck
     <link href="{{ asset('assets/vendors/iCheck/skins/flat/green.css') }}" rel="stylesheet">
 	
-    <!-- bootstrap-progressbar -->
+     bootstrap-progressbar 
     <link href="{{ asset('assets/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet">
-    <!-- JQVMap -->
+     JQVMap 
     <link href="{{ asset('assets/vendors/jqvmap/dist/jqvmap.min.css') }}" rel="stylesheet"/>
-    <!-- bootstrap-daterangepicker -->
+     bootstrap-daterangepicker 
     <link href="{{ asset('assets/vendors/bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
 
-    <!-- Custom Theme Style -->
+     Custom Theme Style 
     <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet">
-  </head>
+  </head> -->
 
   <body class="nav-md">
     <div class="container body">
