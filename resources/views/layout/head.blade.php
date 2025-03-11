01a8263c251b7720a2cc4f4ef2a06fc0d080bee6
JustinG <justin.gestiere@gmail.com>
2025-03-11 08:42:06 +0100

    normalement c'est ok j'ai remis le pat

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -16,7 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	  <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/ico" />
 
-    <title>Click'n Eate</title>
+    <title>Click'n Eat</title>
 
     <!-- Bootstrap -->
     <link href="{{ asset('assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
