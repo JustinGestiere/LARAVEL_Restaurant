b9e23cece603e4f0be66fa1854968caf231b50be
JustinG <justin.gestiere@gmail.com>
2025-03-15 15:17:28 +0100

    test peut etre que ca marche

--- resources/views/layout/leftbar.blade.php
+++ resources/views/layout/leftbar.blade.php
@@ -9,7 +9,7 @@
         <!-- menu profile quick info -->
         <div class="profile clearfix">
             <div class="profile_info">
-            <span>Welco,</span>
+            <span>Welcome,</span>
             <h2>John Doe</h2>
             </div>
         </div>
